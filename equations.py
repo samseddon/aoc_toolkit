c450a9b3490f77020d26616ad8f9d54acf93253c
sseddon <samuel.seddon@gmail.com>
2023-12-06 08:39:15 +0100

    minor tweaks, perhaps requiring a dict is a bit cumbersome

--- equations.py
+++ equations.py
@@ -5,7 +5,7 @@ def line_dict(line, pattern):
     When given a line and pattern returns a dictonary of keys and values
 
     example repeated line:  
-    "1 Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53\n"
+    "1 Card 1: 41 48 83 86 17 | 81 87  9 41 27  4 48 52\n"
     
     example corresponding pattern 
     "Card {card:d}: {winners} | {hand}\n"
@@ -13,9 +13,12 @@ def line_dict(line, pattern):
     example returned dict 
     {'card': 1, 'winners': '41 48 83 86 17', 'hand': '81 87  9 41 27  4 48 52'}
     """
-    pattern = parse.compile(pattern)
-    match = pattern.search(line)                                               
-    return match.named            
+    if line == "\n":
+        pass
+    else:
+        pattern = parse.compile(pattern)
+        match = pattern.search(line)                                               
+        return match.named            
 
 
 def int_array(line_dict, key, delim = " "):
@@ -24,8 +27,8 @@ def int_array(line_dict, key, delim = " "):
     returns a delimited (automatically space delimited string as an array of 
     ints
 
-    example string 83 86  6 31 17  9 48 53
-    returns [83, 86, 6, 31, 17, 9, 48, 53]
+    example string 81 87  9 41 27  4 48 52
+    returns [81, 87, 9, 41, 27, 4, 48, 52]
 
     naturally this would also work for any string input but you'd have to 
     put it in a dict first or edit this function. Sorry.
